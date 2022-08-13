bae290aff102354ab485f4d96a18616bcbe24f15
remvst <remvst@gmail.com>
2022-08-13 14:37:32 -0400

    We haz buggy

--- index.js
+++ index.js
@@ -31,7 +31,13 @@ TWO_PI = PI * 2;
 let can;
 let ctx;
 
-let head;
+let character;
+
+onmousemove = (e) => {
+    const canvasRect = can.getBoundingClientRect();
+    character.target.x = (e.pageX - canvasRect.left) / canvasRect.width * can.width;
+    character.target.y = (e.pageY - canvasRect.top) / canvasRect.height * can.height
+};
 
 window.addEventListener('load', () => {
     can = document.querySelector('canvas');
@@ -40,49 +46,7 @@ window.addEventListener('load', () => {
 
     ctx = can.getContext('2d');
 
-    head = new Node();
-    head.x = 100;
-    head.y = 100;
-
-    let parent = head;
-    let child;
-    for (let i = 0 ; i < 5 ; i++)  {
-        child = new Node(parent);
-
-        const angleTest = new Node(child);
-        angleTest.minAngleOffset = Math.PI / 2 + Math.PI / 8;
-        angleTest.maxAngleOffset = Math.PI / 2 - Math.PI / 8;
-        // angleTest.angleResolutionResolutionSelector = Node.pickFurthest;
-    
-        const angleTest2 = new Node(child);
-        angleTest2.minAngleOffset = Math.PI * 3 / 2 + Math.PI / 8;
-        angleTest2.maxAngleOffset = Math.PI * 3 / 2 - Math.PI / 8;
-        // angleTest2.angleResolutionResolutionSelector = Node.pickFurthest;
-
-        parent = child;
-    }
-
-    // child.x = 260;
-    // child.y = 200 - 60;
-
-    // child.minAngleOffset = Math.PI / 2 + Math.PI / 8;
-    // child.maxAngleOffset = Math.PI / 2 - Math.PI / 8;
-
-    // const angleTest = new Node(parent);
-    // angleTest.x = 150;
-    // angleTest.y = 250;
-    // angleTest.minAngleOffset = Math.PI / 2 + Math.PI / 8;
-    // angleTest.maxAngleOffset = Math.PI / 2 - Math.PI / 8;
-    // // angleTest.angleResolutionResolutionSelector = Node.pickFurthest;
-
-    // const angleTest2 = new Node(parent);
-    // angleTest2.x = 200;
-    // angleTest2.y = 200;
-    // angleTest2.minAngleOffset = Math.PI * 3 / 2 + Math.PI / 8;
-    // angleTest2.maxAngleOffset = Math.PI * 3 / 2 - Math.PI / 8;
-    // // angleTest2.angleResolutionResolutionSelector = Node.pickFurthest;
-
-
+    character = new Character();
     frame();
 });
 
@@ -95,15 +59,14 @@ function frame() {
     lastFrame = now;
     totalTime += elapsed;
 
-    head.x += 50 * elapsed;
+    // head.position.x = can.width / 2 + Math.cos(-totalTime * Math.PI / 16) * 300;
+    // head.position.y = can.width / 2 + Math.sin(-totalTime * Math.PI / 16) * 300;
+    // head.position.x += elapsed * 20;
 
-    head.x = can.width / 2 + Math.cos(totalTime * Math.PI / 8) * 300;
-    head.y = can.width / 2 + Math.sin(totalTime * Math.PI / 8) * 300;
-
-    head.resolve();
+    character.cycle(elapsed);
 
     clear();
-    head.render();
+    character.render();
 
     requestAnimationFrame(frame);
 }
@@ -124,12 +87,73 @@ canvasProto.wrap = function(f) {
 
 let nextId = 1;
 
+class Character {
+    constructor() {
+        this.target = {'x': 0, 'y': 0};
+
+        this.head = new Node();
+        this.head.position.x = 100;
+        this.head.position.y = 100;
+
+        let parent = this.head;
+        let child;
+        for (let i = 0 ; i < 10 ; i++)  {
+            child = new Node(parent);
+            child.minDistanceFromParent = 30;
+            child.maxDistanceFromParent = 50;
+            child.visualSpeed = 100;
+            child.angleResolutionResolutionSelector = Node.pickClosest;
+
+            const leg1 = new Node(child);
+            leg1.minAngleOffset = Math.PI / 2 + Math.PI / 4;
+            leg1.maxAngleOffset = Math.PI / 2 - Math.PI / 4;
+            leg1.minDistanceFromParent = 40;
+            leg1.maxDistanceFromParent = 60;
+            leg1.visualSpeed = 200;
+            leg1.angleResolutionResolutionSelector = Node.pickAverage;
+        
+            const leg2 = new Node(child);
+            leg2.minAngleOffset = Math.PI * 3 / 2 + Math.PI / 4;
+            leg2.maxAngleOffset = Math.PI * 3 / 2 - Math.PI / 4;
+            leg2.minDistanceFromParent = 40;
+            leg2.maxDistanceFromParent = 60;
+            leg2.visualSpeed = 200;
+            leg2.angleResolutionResolutionSelector = Node.pickAverage;
+
+            parent = child;
+        }
+
+        this.head.realign();
+    }
+    
+    cycle(elapsed) {
+        const distToTarget = dist(this.head.position, this.target);
+        const angleToTarget = angleBetween(this.head.position, this.target);
+        const appliedDistance = Math.min(elapsed * 100, distToTarget);
+
+        this.head.position.x += Math.cos(angleToTarget) * appliedDistance;
+        this.head.position.y += Math.sin(angleToTarget) * appliedDistance;
+
+        this.head.cycle(elapsed);
+        this.head.resolve();
+    }
+
+    render() {
+        this.head.render();
+
+        ctx.wrap(() => {
+            ctx.fillStyle = '#f00';
+            ctx.fillRect(this.target.x, this.target.y, 10, 10);
+        });
+    }
+}
+
 class Node {
     constructor(parent) {
         this.id = `n[${nextId++}]`;
 
-        this.x = 0;
-        this.y = 0;
+        this.position = {'x': 0, 'y': 0};
+        this.visualPosition = {'x': 0, 'y': 0};
 
         this.minDistanceFromParent = 50;
         this.maxDistanceFromParent = 70;
@@ -163,24 +187,13 @@ class Node {
             return null;
         }
 
-        return normalize(angleBetween(this, this.parent));
-    }
-
-    get angleOffset() {
-        if (!this.parent || !this.parent.parent) {
-            return null;
-        }
-
-        const { angle } = this;
-        const parentAngle = this.parent.angle;
-        return normalize(angle, parentAngle);
+        return normalize(angleBetween(this.position, this.parent.position));
     }
 
     needsLengthResolution() {
-        const distanceFromParent = dist(this, this.parent);
-        // console.log(distanceFromParent, this.minDistanceFromParent, this.maxDistanceFromParent);
-        if (!isBetween(this.minDistanceFromParent, distanceFromParent, this.maxDistanceFromParent)) return true;
-        return false;
+        if (!this.parent) return false;
+        const distanceFromParent = dist(this.position, this.parent.position);
+        return !isBetween(this.minDistanceFromParent, distanceFromParent, this.maxDistanceFromParent);
     }
 
     needsAngleResolution() {
@@ -191,133 +204,190 @@ class Node {
         const angleOffset = normalize(this.angle - parentAngle);
 
         if (!isBetween(normalize(this.minAngleOffset), angleOffset, normalize(this.maxAngleOffset))) {
-            console.log(normalize(this.minAngleOffset), angleOffset, normalize(this.maxAngleOffset));
             return true;
         }
     }
 
-    resolve() {
-        if (this.parent) {
-            // Resolve length elasticity
-            if (this.needsLengthResolution()) {
-                const angleToParent = angleBetween(this, this.parent);
-                const targetX = this.parent.x - this.minDistanceFromParent * Math.cos(angleToParent);
-                const targetY = this.parent.y - this.minDistanceFromParent * Math.sin(angleToParent);
+    onParentLengthResolved() {
+        // this.resolveAngle();
+    }
 
-                this.x = targetX;
-                this.y = targetY;
-                // const angleToParentAfter = angleBetween(this, this.parent);
-                // console.log(angleToParent, angleToParentAfter);
-            }
+    resolveLength() {
+        const angleToParent = angleBetween(this.position, this.parent.position);
+        const averageDistance = (this.minDistanceFromParent + this.maxDistanceFromParent) / 2;
 
-            if (this.needsAngleResolution()) {
-                console.log('resolving angle for ', this.id);
+        const targetX = this.parent.position.x - averageDistance * Math.cos(angleToParent);
+        const targetY = this.parent.position.y - averageDistance * Math.sin(angleToParent);
 
-                const distanceFromParent = dist(this, this.parent);
-                const { angle } = this;
-                const parentAngle = this.parent.angle;
-                const averageOffset = (this.minAngleOffset + this.maxAngleOffset) / 2;
-                const resolveWithAverageOffset = normalize(parentAngle + averageOffset);
-                const resolveWithMinOffset = normalize(parentAngle + this.minAngleOffset);
-                const resolveWithMaxOffset = normalize(parentAngle + this.maxAngleOffset);
+        this.position.x = targetX;
+        this.position.y = targetY;
 
-                // console.log('angle should be', resolveWithMinOffset);
+        this.children.forEach(child => child.onParentLengthResolved());
+    }
 
-                const resolutionWithMinOffset = {
-                    'x': this.parent.x + Math.cos(resolveWithMinOffset + Math.PI) * distanceFromParent,
-                    'y': this.parent.y + Math.sin(resolveWithMinOffset + Math.PI) * distanceFromParent,
-                };
+    resolveAngle() {
+        const distanceFromParent = dist(this.position, this.parent.position);
+        const parentAngle = this.parent.angle;
+        const averageOffset = (this.minAngleOffset + this.maxAngleOffset) / 2;
+        const resolveWithAverageOffset = normalize(parentAngle + averageOffset);
+        const resolveWithMinOffset = normalize(parentAngle + this.minAngleOffset);
+        const resolveWithMaxOffset = normalize(parentAngle + this.maxAngleOffset);
 
-                const resolutionWithMaxOffset = {
-                    'x': this.parent.x + Math.cos(resolveWithMaxOffset + Math.PI) * distanceFromParent,
-                    'y': this.parent.y + Math.sin(resolveWithMaxOffset + Math.PI) * distanceFromParent,
-                }
+        const resolutionWithMinOffset = {
+            'x': this.parent.position.x + Math.cos(resolveWithMinOffset + Math.PI) * distanceFromParent,
+            'y': this.parent.position.y + Math.sin(resolveWithMinOffset + Math.PI) * distanceFromParent,
+        };
 
-                const resolutionWithAverageOffset = {
-                    'x': this.parent.x + Math.cos(resolveWithAverageOffset + Math.PI) * distanceFromParent,
-                    'y': this.parent.y + Math.sin(resolveWithAverageOffset + Math.PI) * distanceFromParent,
-                };
-
-                const resolutions = [
-                    resolutionWithMinOffset,
-                    resolutionWithMaxOffset,
-                    resolutionWithAverageOffset,
-                ].sort((a, b) => dist(this, a) - dist(this, b));
-
-                // const resolution = dist(this, resolutionWithMinOffset) > dist(this, resolutionWithMaxOffset) ? resolutionWithMinOffset : resolutionWithMaxOffset;
-                // const resolution = resolutionWithAverageOffset;
-
-                const resolution = this.angleResolutionResolutionSelector(resolutions);
-                // const resolution = resolutionWithAverageOffset;
-                
-                this.x = resolution.x;
-                this.y = resolution.y;
-
-                const newAngle = this.angle;
-
-                console.log('angle should be', resolveWithAverageOffset, ' but is', newAngle);
-            }
+        const resolutionWithMaxOffset = {
+            'x': this.parent.position.x + Math.cos(resolveWithMaxOffset + Math.PI) * distanceFromParent,
+            'y': this.parent.position.y + Math.sin(resolveWithMaxOffset + Math.PI) * distanceFromParent,
         }
 
+        const resolutionWithAverageOffset = {
+            'x': this.parent.position.x + Math.cos(resolveWithAverageOffset + Math.PI) * distanceFromParent,
+            'y': this.parent.position.y + Math.sin(resolveWithAverageOffset + Math.PI) * distanceFromParent,
+        };
+
+        const angleOffset = normalize(this.angle - parentAngle);
+
+        const resolutions = [
+            resolutionWithMinOffset,
+            resolutionWithMaxOffset,
+            resolutionWithAverageOffset,
+        ].sort((a, b) => {
+            const angleOffsetA = normalize(angleBetween(a, this.parent.position) - parentAngle) - angleOffset;
+            const angleOffsetB = normalize(angleBetween(b, this.parent.position) - parentAngle) - angleOffset;
+            return Math.abs(angleOffsetA) - Math.abs(angleOffsetB);
+        });
+
+        // const resolutionOffsets = resolutions.map((a) => {
+        //     return normalize(angleBetween(a, this.parent.position) - parentAngle);
+        // })
+
+        const resolution = this.angleResolutionResolutionSelector(resolutions);
+        
+        this.position.x = resolution.x;
+        this.position.y = resolution.y;
+
+        const newAngleOffset = normalize(this.angle - parentAngle);
+        // console.log('resolved angle', angleOffset, newAngleOffset, resolutionOffsets);
+
+        // if (this.id === 'n[3]') throw new Error();
+    }
+
+    resolve() {
+        // Resolve length elasticity
+        if (this.needsLengthResolution()) {
+            this.resolveLength();
+        }
+
+        // Resolve angle elasticity
+        if (this.needsAngleResolution()) {
+            this.resolveAngle();
+        }
+    }
+
+    realign() {
+        this.visualPosition.x = this.position.x;
+        this.visualPosition.y = this.position.y;
         for (const child of this.children) {
-            child.resolve();
+            child.realign();
+        }
+    }
+
+    cycle(elapsed) {
+        const angleToTarget = angleBetween(this.visualPosition, this.position);
+        let distanceToTarget = dist(this.visualPosition, this.position) * 0.2;
+        
+        this.visualPosition.x += distanceToTarget * Math.cos(angleToTarget);
+        this.visualPosition.y += distanceToTarget * Math.sin(angleToTarget);
+
+        this.resolve();
+
+        for (const child of this.children) {
+            child.cycle(elapsed);
         }
     }
 
     render() {
-        ctx.fillStyle = '#fff';
-        ctx.fillRect(this.x - 5, this.y - 5, 10, 10);
+        ctx.wrap(() => {
+            ctx.translate(this.visualPosition.x, this.visualPosition.y);
 
-        if (this.parent) {
-            ctx.lineWidth = 2;
-            ctx.strokeStyle = '#fff'
-            ctx.beginPath();
-            ctx.moveTo(this.x, this.y);
-            ctx.lineTo(this.parent.x, this.parent.y);
-            ctx.stroke();
-        }
+            ctx.fillStyle = '#f00';
+            ctx.fillRect(-5, -5, 10, 10);
 
-        const { angle } = this;
-        if (angle) {
-            const length = 30;
-
-            ctx.lineWidth = 2;
-            ctx.strokeStyle = '#f00'
-            ctx.beginPath();
-            ctx.moveTo(this.x, this.y);
-            ctx.lineTo(this.x + Math.cos(this.angle) * length, this.y + Math.sin(this.angle) * length);
-            ctx.stroke();
-
-            ctx.wrap(() => {
-                ctx.translate(this.x, this.y);
-                // ctx.rotate(this.angle);
-
-                if (!this.parent || !this.parent.parent) return;
-                const parentAngle = this.parent.angle;
-
-                ctx.lineWidth = 2;
-                ctx.strokeStyle = '#0f0'
+            if (this.parent) {
+                ctx.lineWidth = 10;
+                ctx.strokeStyle = '#fff';
+                ctx.lineCap = 'round';
                 ctx.beginPath();
                 ctx.moveTo(0, 0);
-                ctx.lineTo(Math.cos(this.maxAngleOffset + this.parent.angle) * length, Math.sin(this.maxAngleOffset + this.parent.angle) * length);
-                ctx.moveTo(0, 0);
-                ctx.lineTo(Math.cos(this.minAngleOffset + this.parent.angle) * length, Math.sin(this.minAngleOffset + this.parent.angle) * length);
+                ctx.lineTo(this.parent.visualPosition.x - this.visualPosition.x, this.parent.visualPosition.y - this.visualPosition.y);
                 ctx.stroke();
-            });
+            }
+        });
 
-            // if (this.id === 'n[4]') {
-            //     // console.log(angle);
-            // }
-        }
-
-        ctx.fillStyle = '#fff';
-        ctx.font = '12pt Arial';
-        ctx.textAlign = 'left';
-        ctx.textBaseline = 'bottom';
-        ctx.fillText(this.id, this.x, this.y - 10);
+        ctx.wrap(() => {
+            ctx.globalAlpha *= 0.5;
+            this.renderDebug();
+        });
 
         for (const child of this.children) {
             ctx.wrap(() => child.render());
-        }
+        } 
+    }
+
+    renderDebug() {
+        ctx.wrap(() => {
+            ctx.translate(this.position.x, this.position.y);
+
+            ctx.fillStyle = '#fff';
+            // ctx.fillRect(-5, -5, 10, 10);
+
+            if (this.parent) {
+                ctx.lineWidth = 2;
+                ctx.strokeStyle = '#fff'
+                ctx.beginPath();
+                ctx.moveTo(0, 0);
+                ctx.lineTo(this.parent.position.x - this.position.x, this.parent.position.y - this.position.y);
+                ctx.stroke();
+            }
+
+            const { angle } = this;
+            if (angle) {
+                const length = 30;
+
+                ctx.lineWidth = 2;
+                ctx.strokeStyle = '#f00'
+                ctx.beginPath();
+                ctx.moveTo(0, 0);
+                ctx.lineTo(cos(this.angle) * length, sin(this.angle) * length);
+                ctx.stroke();
+
+                ctx.wrap(() => {
+                    if (!this.parent || !this.parent.parent) return;
+                    const parentAngle = this.parent.angle;
+
+                    ctx.lineWidth = 2;
+                    ctx.strokeStyle = '#0f0'
+                    ctx.beginPath();
+                    ctx.moveTo(0, 0);
+                    ctx.lineTo(Math.cos(this.maxAngleOffset + this.parent.angle) * length, Math.sin(this.maxAngleOffset + this.parent.angle) * length);
+                    ctx.moveTo(0, 0);
+                    ctx.lineTo(Math.cos(this.minAngleOffset + this.parent.angle) * length, Math.sin(this.minAngleOffset + this.parent.angle) * length);
+                    ctx.stroke();
+                });
+
+                // if (this.id === 'n[4]') {
+                //     // console.log(angle);
+                // }
+            }
+
+            ctx.fillStyle = '#fff';
+            ctx.font = '12pt Arial';
+            ctx.textAlign = 'left';
+            ctx.textBaseline = 'bottom';
+            ctx.fillText(this.id, 0, -10);
+        });
     }
 }
