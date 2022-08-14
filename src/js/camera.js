2fc774802108b5966c3be82c4e41e7f1c4ec94a7
remvst <remvst@gmail.com>
2022-08-13 22:42:34 -0400

    Moving camera

--- src/js/camera.js
+++ src/js/camera.js
@@ -13,6 +13,17 @@ class Camera {
     }
 
     cycle(elapsed) {
-        // TODO
+        const target = {
+            'x':player.head.position.x - CANVAS_WIDTH / 2,
+            'y':player.head.position.y - CANVAS_HEIGHT / 2,
+        };
+
+        const distanceToTarget = dist(target, this),
+            speed = max(1, distanceToTarget / 0.2),
+            angle = atan2(target.y - this.y, target.x - this.x),
+            appliedDist = min(speed * elapsed, distanceToTarget);
+
+        this.x += cos(angle) * appliedDist;
+        this.y += sin(angle) * appliedDist;
     }
 }
