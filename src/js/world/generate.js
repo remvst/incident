8762b536a4dd20166fdd08ea8d9efc51bb4b9b03
remvst <remvst@gmail.com>
2022-08-14 11:16:34 -0400

    Slightly more interesting map

--- src/js/world/generate.js
+++ src/js/world/generate.js
@@ -1,24 +1,13 @@
 generateFromCanvas = (can, ctx) => {
     const grid = [];
-
     const imageData = ctx.getImageData(0, 0, can.width, can.height);
 
     for (let y = 0 ; y < can.height ; y++) {
         grid.push([]);
         for (let x = 0 ; x < can.width ; x++) {
             grid[y][x] = imageData.data[y * can.width * 4 + x * 4] > 0x80;
-            imageData.data[y * can.width * 4 + x * 4] = grid[y][x] * 255;
-            imageData.data[y * can.width * 4 + x * 4 + 1] = grid[y][x] * 255;
-            imageData.data[y * can.width * 4 + x * 4 + 2] = grid[y][x] * 255;
-            imageData.data[y * can.width * 4 + x * 4 + 3] = grid[y][x] * 255;
         }
     }
-
-    ctx.putImageData(imageData, 0, 0);
-
-    ctx.globalCompositeOperation = nomangle('source-atop');
-    ctx.fillStyle = '#048';
-    ctx.fillRect(0, 0, can.width, can.height);
 
     return grid;
 };
@@ -50,24 +39,29 @@ generateRandomWorld = () => createCanvas(50, 50, (ctx, can) => {
 
     ctx.fillRect(0, 0, can.width, can.height);
 
-    ctx.clearRect(4, 4, 3, 3);
+    ctx.fillStyle = '#000';
+    ctx.fillRect(4, 4, 3, 3);
 
-    for (let x = 5 ; x < can.width ; x += 5) {
-        clearPath(x, 5, x, can.height - 5);
-        clearPath(5, x, can.height - 5, x);
+    for (let x = 1 ; x < can.width ; x += 10) {
+        for (let y = 1 ; y < can.height ; y += 10) {
+
+            w = rnd(4, 9);
+            h = rnd(4, 9);
+
+            ctx.fillRect(x, y, w, h);
+
+            ctx.fillRect(x, ~~rnd(y, y + h - 2), 10, 2);
+            ctx.fillRect( ~~rnd(x, x + w - 2), y, 2, 10);
+        }
     }
 
     // Close the arena
-    ctx.fillRect(0, 0, can.width, 1);
-    ctx.fillRect(0, 0, 1, can.height);
-    ctx.fillRect(0, can.height - 1, can.width, 1);
-    ctx.fillRect(can.width - 1, 0, 1, can.height);
-
-    // Open up the center (for the menu)
-    ctx.fillStyle = '#000';
-    ctx.beginPath();
-    ctx.arc(25, 25, 4, 0, TWO_PI);
-    ctx.fill();
+    ctx.fillStyle = '#fff';
+    const thickness = 3;
+    ctx.fillRect(0, 0, can.width, thickness);
+    ctx.fillRect(0, 0, thickness, can.height);
+    ctx.fillRect(0, can.height - thickness, can.width, thickness);
+    ctx.fillRect(can.width - thickness, 0, thickness, can.height);
 
     return generateFromCanvas(can, ctx);
 });
