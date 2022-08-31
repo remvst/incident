14f1a06e006f75da948300fc3e641d0cb12fa36d
remvst <remvst@gmail.com>
2022-08-31 02:10:55 -0400

    Support gamepad controls

--- src/js/input.js
+++ src/js/input.js
@@ -1,24 +1,54 @@
 updateControls = () => {
+    // Detect if the user is using a gamepad at all
+    if (navigator.getGamepads && inputMode !== INPUT_MODE_GAMEPAD) {
+        for (const gamepad of navigator.getGamepads()) {
+            if (gamepad) {
+                for (const button of gamepad.buttons) {
+                    if (button.pressed) {
+                        inputMode = INPUT_MODE_GAMEPAD;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
     if (inputMode === INPUT_MODE_MOUSE) {
         player.target.x = mousePosition.x + camera.x;
         player.target.y = mousePosition.y + camera.y;
         player.movementPower = mouseDown ? 1 : 1 / 4;
     } else if (inputMode === INPUT_MODE_TOUCH) {
         if (!hasTouchDown) {
-            console.log('go touch');
             player.target.x = player.head.position.x;
             player.target.y = player.head.position.y;
         } else {
             const angle = angleBetween(touchStartPosition, touchPosition);
             const force = dist(touchStartPosition, touchPosition) / TOUCH_JOYSTICK_MAX_RADIUS;
 
-            player.target.x = player.head.position.x + cos(angle) * 100;
-            player.target.y = player.head.position.y + sin(angle) * 100;
+            player.target.x = player.head.position.x + cos(angle) * force * 100;
+            player.target.y = player.head.position.y + sin(angle) * force * 100;
 
             player.movementPower = force;
         }
     } else if (inputMode === INPUT_MODE_GAMEPAD) {
+        for (const gamepad of navigator.getGamepads()) {
+            if (gamepad) {
+                const angle = atan2(gamepad.axes[1], gamepad.axes[0]);
+                let force = distP(0, 0, gamepad.axes[0], gamepad.axes[1]) * 1 / 4;
 
+                for (const button of gamepad.buttons) {
+                    if (button.pressed) {
+                        force = 1;
+                        break;
+                    }
+                }
+
+                player.target.x = player.head.position.x + cos(angle) * 100;
+                player.target.y = player.head.position.y + sin(angle) * 100;
+
+                player.movementPower = force;
+            }
+        }
     }
 };
 
